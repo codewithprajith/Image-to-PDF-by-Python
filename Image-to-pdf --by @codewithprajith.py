fe9b167406c066ee84a83faf72f075e2850b08be
Code With Prajith <codewithprajith@gmail.com>
2022-02-15 14:58:40 +0530

    Add files via upload

---
+++ Image-to-pdf --by @codewithprajith.py
@@ -0,0 +1,9 @@
+#follow @codewithprajith for more projects
+from PIL import Image
+
+image = Image.open('sample.PNG')
+im = image.convert('RGB')
+
+im.save('sample.png output to pdf.pdf')
+
+#follow @codewithprajith for more projects
